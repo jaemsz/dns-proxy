7e8975f745277b8fc36a513760d89d9dfadbc520
jaemszy424@gmail.com <jaemszy424@gmail.com>
2022-05-10 18:41:41 -0400

    Changed localhost to 127.0.0.1

--- proxy/src/main.js
+++ proxy/src/main.js
@@ -3,7 +3,7 @@ const dns = require("native-dns");
 const asyncLib = require("async");
 const mongo = require("mongodb");
 
-const mongoUrl = process.env.DNS_MONGO_URL || "mongodb://localhost:27017/";
+const mongoUrl = process.env.DNS_MONGO_URL || "mongodb://127.0.0.1:27017/";
 const mongoClient = mongo.MongoClient;
 let mongoDb = null;
 
